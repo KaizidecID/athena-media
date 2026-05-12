043d525ef44e97a9f741a6fdd8f5ad213224b919
KaizidecID <rozezjukutgoreng05@gmail.com>
2026-05-13 03:09:10 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,83 @@
 const express = require('express')
 const path = require('path')
+const yts = require('yt-search')
 
 const app = express()
 const PORT = 3000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req,res)=>{
-res.sendFile(path.join(__dirname,'views/dashboard.html'))
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'dashboard.html'))
 })
 
-app.get('/tiktok',(req,res)=>{
-res.sendFile(path.join(__dirname,'views/tiktok.html'))
+app.get('/tiktok', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'tiktok.html'))
 })
 
-app.get('/instagram',(req,res)=>{
-res.sendFile(path.join(__dirname,'views/instagram.html'))
+app.get('/instagram', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'instagram.html'))
 })
 
-app.get('/spotify',(req,res)=>{
-res.sendFile(path.join(__dirname,'views/spotify.html'))
+app.get('/spotify', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'spotify.html'))
 })
 
-app.get('/ytmp3',(req,res)=>{
-res.sendFile(path.join(__dirname,'views/ytmp3.html'))
+app.get('/ytmp3', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'ytmp3.html'))
 })
 
-app.get('/musik',(req,res)=>{
-res.sendFile(path.join(__dirname,'views/musik.html'))
+app.get('/musik', (req, res) => {
+res.sendFile(path.join(__dirname, 'views', 'musik.html'))
 })
 
-app.listen(PORT, ()=>{
-console.log(`ATHENA running on http://localhost:${PORT}`)
+app.get('/api/music', async (req,res)=>{
+
+try{
+
+let q = req.query.q
+
+if(!q){
+
+return res.json({
+status:false,
+msg:'query kosong'
+})
+
+}
+
+let search = await yts(q)
+
+let video = search.videos[0]
+
+if(!video){
+
+return res.json({
+status:false,
+msg:'music tidak ditemukan'
+})
+
+}
+
+res.json({
+status:true,
+title:video.title,
+thumbnail:video.thumbnail,
+url:video.url,
+author:video.author.name
+})
+
+}catch(e){
+
+res.json({
+status:false,
+msg:'server error'
+})
+
+}
+
+})
+
+app.listen(PORT, () => {
+console.log(`Running http://localhost:${PORT}`)
 })
